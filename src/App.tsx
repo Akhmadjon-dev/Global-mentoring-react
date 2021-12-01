54931ec378dfc605311467bb7e8190882407226a
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-12-01 19:02:25 +0500

    prettier formatted

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,17 @@ import Header from "./containers/Header";
 import Movies from "./containers/Movies";
 import { StyledApp } from "./styles/App.styled";
 import MovieDetails from "./components/MovieDetails";
-import { useAppDispatch } from './store/hooks';
-import { addMovie, deleteMovie, fetchMovies, filterMovies, searchMovies, sortByMovies, updateMovie } from './store/thunks';
-import { selectMovies } from './store/movies/moviesSlice';
+import { useAppDispatch } from "./store/hooks";
+import {
+  addMovie,
+  deleteMovie,
+  fetchMovies,
+  filterMovies,
+  searchMovies,
+  sortByMovies,
+  updateMovie,
+} from "./store/thunks";
+import { selectMovies } from "./store/movies/moviesSlice";
 import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import Main from "./containers/Main";
 import Search from "./containers/Sarch";
@@ -17,21 +25,20 @@ import Search from "./containers/Sarch";
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
   const [isEditable, setIsEditable] = useState<boolean>(false);
-  const [moivieIdForUpdate, setMoivieIdForUpdate] = useState<string>('');
+  const [moivieIdForUpdate, setMoivieIdForUpdate] = useState<string>("");
   const [data, setData] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState<IMovie | null>();
   const dispatch: any = useAppDispatch();
-  const history :any = useHistory()
+  const history: any = useHistory();
   // useEffect(() => {
   //   dispatch(fetchMovies());
   // }, [dispatch]);
 
-
   const convertMovies = useSelector(selectMovies);
 
   const deleteHandler = (id: any) => {
-    dispatch(deleteMovie(id))
-    dispatch(fetchMovies())
+    dispatch(deleteMovie(id));
+    dispatch(fetchMovies());
   };
 
   const converMovieToRequest = (movie) => {
@@ -44,25 +51,25 @@ function App() {
       budget: +movie.budget,
       revenue: +movie.revenue,
       runtime: +movie.runtime,
-    }
-    return body
-  }
+    };
+    return body;
+  };
 
   const addHandler = (movie) => {
-    const body = converMovieToRequest(movie)
+    const body = converMovieToRequest(movie);
     dispatch(addMovie(body));
   };
 
   const getMovieIdForUpdate = (id: string) => {
-    setModalIsOpen(true)
-    setIsEditable(true)
-    setMoivieIdForUpdate(id)
+    setModalIsOpen(true);
+    setIsEditable(true);
+    setMoivieIdForUpdate(id);
   };
 
   const editHandler = (values) => {
-    console.log(values, 'editHandler');
-    const body = { ...values, genres: [values.genres] }
-    dispatch(updateMovie(body))
+    console.log(values, "editHandler");
+    const body = { ...values, genres: [values.genres] };
+    dispatch(updateMovie(body));
   };
 
   const filterHandler = (id: string): any => {
@@ -83,46 +90,52 @@ function App() {
   };
 
   const sortHandler = (id: string): any => {
-    history.push('/search/sortBy?'+ id);
+    history.push("/search/sortBy?" + id);
   };
 
   const searchHandler = (e: any): any => {
     e.preventDefault();
     const searchValue = e.target[0].value;
-    history.push('/search/'+ searchValue);
+    history.push("/search/" + searchValue);
   };
 
   return (
     <StyledApp>
       <Switch>
-        <Route path="/search" render={(props) => (
-          <Main  
-            {...props} 
-            searchHandler={searchHandler} 
-            modalOpen={() => setModalIsOpen(true)}  
-            selectMovieHandler={selectedMovieHandler}
-            filterMovies={filterHandler}
-            edit={getMovieIdForUpdate}
-            add={addHandler}
-            deleteHandler={deleteHandler}
-            data={convertMovies}
-            sortMovies={sortHandler}
-          />
-          )} />
-        <Route path="/search/:query" render={(props) => (
-          <Search  
-            {...props} 
-            searchHandler={searchHandler} 
-            modalOpen={() => setModalIsOpen(true)}  
-            selectMovieHandler={selectedMovieHandler}
-            filterMovies={filterHandler}
-            edit={getMovieIdForUpdate}
-            add={addHandler}
-            deleteHandler={deleteHandler}
-            data={convertMovies}
-            sortMovies={sortHandler}
-          />
-          )} />
+        <Route
+          path="/search"
+          render={(props) => (
+            <Main
+              {...props}
+              searchHandler={searchHandler}
+              modalOpen={() => setModalIsOpen(true)}
+              selectMovieHandler={selectedMovieHandler}
+              filterMovies={filterHandler}
+              edit={getMovieIdForUpdate}
+              add={addHandler}
+              deleteHandler={deleteHandler}
+              data={convertMovies}
+              sortMovies={sortHandler}
+            />
+          )}
+        />
+        <Route
+          path="/search/:query"
+          render={(props) => (
+            <Search
+              {...props}
+              searchHandler={searchHandler}
+              modalOpen={() => setModalIsOpen(true)}
+              selectMovieHandler={selectedMovieHandler}
+              filterMovies={filterHandler}
+              edit={getMovieIdForUpdate}
+              add={addHandler}
+              deleteHandler={deleteHandler}
+              data={convertMovies}
+              sortMovies={sortHandler}
+            />
+          )}
+        />
         {/* <Route path="/movies" render={(props) => (
             <Movies
               {...props}
--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,77 +1,78 @@
-import Header  from './Header/index'
-import React from 'react'
-import Movies from './Movies'
-import { useAppDispatch } from '../store/hooks'
-import { fetchMovies, filterMovies as filterMovieByGenre, searchMovies, SearchPopularMovies, sortByMovies } from '../store/thunks'
-import { useSelector } from 'react-redux'
-import { selectMovies } from '../store/movies/moviesSlice'
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import Header from "./Header/index";
+import React from "react";
+import Movies from "./Movies";
+import { useAppDispatch } from "../store/hooks";
+import {
+  fetchMovies,
+  filterMovies as filterMovieByGenre,
+  searchMovies,
+  SearchPopularMovies,
+  sortByMovies,
+} from "../store/thunks";
+import { useSelector } from "react-redux";
+import { selectMovies } from "../store/movies/moviesSlice";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function Main({
-    searchHandler,
-    modalOpen,
-    selectMovieHandler,
-    filterMovies,
-    edit,
-    add,
-    deleteHandler,
-    sortMovies
-}:{
-    searchHandler: (e: any) => void;
-    modalOpen: any;
-    selectMovieHandler: any;
-    filterMovies: any;
-    edit: any;
-    add: any;
-    deleteHandler: any;
-    data: any;
-    sortMovies: any;
+  searchHandler,
+  modalOpen,
+  selectMovieHandler,
+  filterMovies,
+  edit,
+  add,
+  deleteHandler,
+  sortMovies,
+}: {
+  searchHandler: (e: any) => void;
+  modalOpen: any;
+  selectMovieHandler: any;
+  filterMovies: any;
+  edit: any;
+  add: any;
+  deleteHandler: any;
+  data: any;
+  sortMovies: any;
 }) {
-    const dispatch: any = useAppDispatch()
-    const location: any = useLocation();
-    const query = location.pathname.split('/')[2]
-    let filterType = location.search.split('').slice(1).join("")
-    const search = location.search
-    useEffect(() => {
-        dispatch(SearchPopularMovies())
-    }, [])
+  const dispatch: any = useAppDispatch();
+  const location: any = useLocation();
+  const query = location.pathname.split("/")[2];
+  let filterType = location.search.split("").slice(1).join("");
+  const search = location.search;
+  useEffect(() => {
+    dispatch(SearchPopularMovies());
+  }, []);
 
-    useEffect(() => {
-        if(location.pathname.includes('genre')){
-            
-            if (filterType === "all") {
-                dispatch(fetchMovies());
-              }
-              else {
-                  dispatch(filterMovieByGenre(filterType));
-                }
-                
-            }
-            else if(location.pathname.includes('sortBy')){
-              dispatch(sortByMovies(filterType));
-            }
-            else{
-            dispatch(searchMovies(query));
-        }
-    }, [query, search])
+  useEffect(() => {
+    if (location.pathname.includes("genre")) {
+      if (filterType === "all") {
+        dispatch(fetchMovies());
+      } else {
+        dispatch(filterMovieByGenre(filterType));
+      }
+    } else if (location.pathname.includes("sortBy")) {
+      dispatch(sortByMovies(filterType));
+    } else {
+      dispatch(searchMovies(query));
+    }
+  }, [query, search]);
 
-    const data = useSelector(selectMovies);
+  const data = useSelector(selectMovies);
 
-    return (
-        <React.Fragment>
-            <Header searchHandler={searchHandler} modalOpen={modalOpen} />
-            <Movies 
-                selectMovieHandler={selectMovieHandler} 
-                filterMovies={filterMovies} 
-                edit={edit}
-                add={add}
-                deleteHandler={deleteHandler}
-                data={data}
-                sortMovies={sortMovies}
-            />
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Header searchHandler={searchHandler} modalOpen={modalOpen} />
+      <Movies
+        selectMovieHandler={selectMovieHandler}
+        filterMovies={filterMovies}
+        edit={edit}
+        add={add}
+        deleteHandler={deleteHandler}
+        data={data}
+        sortMovies={sortMovies}
+      />
+    </React.Fragment>
+  );
 }
 
-export default Main
+export default Main;
